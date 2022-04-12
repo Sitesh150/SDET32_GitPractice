4a22ade0ed742be964151be77890ecb7011c7e92
Admin <Admin@DESKTOP-9EEG1QL>
2022-04-12 14:28:24 +0530

    conflict First

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
@@ -9,6 +9,7 @@ public class ClassC {
 	    System.out.println("Sixth push");
 	    System.out.println("Seventhpush Push");
 	    System.out.println("ninethpush Push");
+	    System.out.println("tenthhpush Push");
 
     }
 }
