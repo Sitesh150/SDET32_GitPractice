462447f4c5275abd31a28b13fcce81ed2bdea40b
Admin <Admin@DESKTOP-9EEG1QL>
2022-04-12 12:36:31 +0530

    commit2

---
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32_GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+@Test
+public void demoTest2() {
+	System.out.println("Push 2");
+	System.out.println("push 3");
+  }
+}
