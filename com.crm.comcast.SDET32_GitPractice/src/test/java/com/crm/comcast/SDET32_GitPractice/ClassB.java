93b5b31bfc752125f6065f1dfc2190739faa0429
Sitesh <89698976+Sitesh150@users.noreply.github.com>
2022-04-13 19:48:58 +0530

    Update ClassB.java

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassB.java
@@ -7,7 +7,7 @@ public class ClassB {
 public void demoTest2() {
 	System.out.println("Push 2");
 	System.out.println("Push 5");
-
+	System.out.println("Push 6");
 	System.out.println("push 3");
   }
 }
